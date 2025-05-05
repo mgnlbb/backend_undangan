55e63b8cbb43ce34b16c3bc24e90ebd20c1f7ec7
mugnilabib <mlabib2122@gmail.com>
2025-05-05 07:51:07 +0700

    fixing index.php

--- api/index.php
+++ api/index.php
@@ -6,15 +6,4 @@
  * Vercel + PHP
  */
 
-// Tambahkan ini di baris paling atas
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, x-access-key");
-
-// Untuk menangani preflight request (OPTIONS)
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
-
 require_once __DIR__ . '/../public/index.php';
